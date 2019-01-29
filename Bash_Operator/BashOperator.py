f1434da7ef7c05d8e92a98f62e39a5d51724c8b6
Ashish Kumar <iashishhere@gmail.com>
2019-01-29 13:13:53 +0530

    Create BashOperator.py

---
+++ Bash_Operator/BashOperator.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import datetime
+import logging
+import os
+from airflow import models
+from airflow.operators.dummy_operator import DummyOperator
+
+yesterday = datetime.datetime.combine(
+    datetime.datetime.today() - datetime.timedelta(1),
+    datetime.datetime.min.time())
+
+default_dag_args = {
+
+    # Setting start date as yesterday starts the DAG immediately when it is
+    # detected in the Cloud Storage bucket.
+    'start_date': yesterday,
+    # To email on failure or retry set 'email' arg to your email and enable
+    # emailing here.
+    'email_on_failure': False,
+    'email_on_retry': False,
+    # If a task fails, retry it once after waiting at least 5 minutes
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+    'project_id': models.Variable.get('gcp_project')
+}
+
+#create dag
+with models.DAG(
+        'Bash_operations',
+        # Continue to run DAG once per day
+        schedule_interval=datetime.timedelta(days=1),
+        default_args=default_dag_args) as dag:
+		
+		t1 = BashOperator(
+        task_id= 'create_file',
+        bash_command=create_command,
+        dag=dag)
+		
